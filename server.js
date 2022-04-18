6308a6df2a433369c42e68d484010b05f9de12e3
AntonioMHCC <amoruno@cloudcoachers.com>
2022-04-18 17:53:23 +0200

    change server set

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require('cors');
-const path = require("path");  
-
+const express = require('express');
 const app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.use(cors());
-app.use(express.static(path.join(__dirname,'images','teamsIcons')));  
-
+ 
 app.get('/', (req, res) => {
-    res
-      .status(200)
-      .send('Hello server is running')
-      .end();
-  });
-app.use("/api/players",require('./src/routes/players.routes'))
-app.use("/api/teams",require('./src/routes/teams.routes'))
-app.use("/api/matches",require('./src/routes/matches.routes'))
-
-
-module.exports = app;
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
+});
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
