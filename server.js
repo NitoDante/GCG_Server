0bd27aed4ab8349f98ae0ecef0f0e45b487aacbf
AntonioMHCC <amoruno@cloudcoachers.com>
2022-04-18 17:50:08 +0200

    change server set

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'images','teamsIcons')));  
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
     res
@@ -24,8 +20,5 @@ app.use("/api/players",require('./src/routes/players.routes'))
 app.use("/api/teams",require('./src/routes/teams.routes'))
 app.use("/api/matches",require('./src/routes/matches.routes'))
 
-app.get("/", (req, res) => {
-    res.send("solicitando ruta raiz");
-});
 
 module.exports = app;
